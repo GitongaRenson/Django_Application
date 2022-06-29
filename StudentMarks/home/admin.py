519b55c5124be79b5fe52f9192e8d475f8be1758
GitongaRenson <blackhat.coded@gmail.com>
2022-06-30 00:01:11 +0300

    Django admmin configuration and tables Display

--- StudentMarks/home/admin.py
+++ StudentMarks/home/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import StudentNames
 
 # Register your models here.
+admin.site.site_header = 'STUDENTMARKS'
+admin.site.site_title = 'Welcome TO StudenMarks Portal'
+admin.site.index_title = 'Admin Portal'
+
+
+
+class AdminCUstomerNames(admin.ModelAdmin):
+  list_display = ['id','name','email','gender','course','phone_number']
+  list_display_links = ['id']
+  list_per_page = 5
+  search_fields =['name','email','phone_number','course','gender']
+  list_filter = ['gender','course']
+
+
+
+  class Meta:
+    model = StudentNames
+
+
+admin.site.register(StudentNames,AdminCUstomerNames)
--- StudentMarks/home/views.py
+++ StudentMarks/home/views.py
@@ -1,9 +1,23 @@
 import re
 from urllib import request
 from django.shortcuts import render
+from .models import StudentNames
+from .tables import StudentTable
+from django_tables2 import RequestConfig
+
 
 # Create your views here.
+
+#This is the default root index for the application and should be accessed from the browser.
+
 def index(request):
     calc = 45 + 20
     name = 'My name is tech'
-    return render(request,'home/index.html',{'name':name,'calc':calc})
+
+    student_names = StudentNames.objects.all()
+    student_table = StudentTable(student_names)
+    RequestConfig(request,paginate={'per_page':3}).configure(student_table)
+    args = {'name':name,'calc':calc,'student_names':student_names,'student_table':student_table}
+    return render(request,'home/index.html',args)
+
+
